0622f88fae100dcad388e44e376d4e83b8796013
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-02-13 15:51:52 +0530

    add TicketController

---
+++ src/controllers/TicketController.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import dtos.IssueTicketRequestDTO;
+import dtos.IssueTicketResponseDTO;
+import enums.ResponseStatus;
+import exceptions.GateNotFoundException;
+import models.Ticket;
+import services.TicketService;
+
+public class TicketController {
+    private TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    public IssueTicketResponseDTO issueTicket(IssueTicketRequestDTO requestDTO) {
+        IssueTicketResponseDTO responseDTO = new IssueTicketResponseDTO();
+        Ticket ticket;
+
+        try {
+            ticket = ticketService.issueTicket(requestDTO.getGateId(), requestDTO.getVehicleNumber(), requestDTO.getVehicleType(), requestDTO.getOwnerName());
+        } catch (GateNotFoundException gateNotFoundException) {
+            responseDTO.setResponseStatus(ResponseStatus.FAILED);
+            responseDTO.setFailureMessage(gateNotFoundException.getMessage());
+            return responseDTO;
+        }
+        catch (Exception e) {
+            responseDTO.setResponseStatus(ResponseStatus.FAILED);
+            responseDTO.setFailureMessage(e.getMessage());
+            return responseDTO;
+        }
+
+        responseDTO.setTicket(ticket);
+        responseDTO.setResponseStatus(ResponseStatus.SUCCESS);
+
+        return responseDTO;
+    }
+}

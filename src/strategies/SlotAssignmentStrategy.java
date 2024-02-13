8ecd7db299bbfdebb99f6ec2fe07da42277d3be7
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-02-13 22:08:09 +0530

    modified SlotAssignmentStrategy

--- src/strategies/SlotAssignmentStrategy.java
+++ src/strategies/SlotAssignmentStrategy.java
@@ -2,10 +2,10 @@ package strategies;
 
 import enums.VehicleType;
 import exceptions.ParkingLotNotFoundException;
+import models.ParkingLot;
 import models.ParkingSlot;
-
 import java.util.Optional;
 
 public interface SlotAssignmentStrategy {
-    public Optional<ParkingSlot> getSlot(VehicleType vehicleType, Long gateId) throws ParkingLotNotFoundException;
+    Optional<ParkingSlot> getSlot(VehicleType vehicleType, ParkingLot parkingLot) throws ParkingLotNotFoundException;
 }

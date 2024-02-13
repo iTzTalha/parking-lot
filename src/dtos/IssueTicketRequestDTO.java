17644b362e8de4111d1ea4cb932a9c3d396ae199
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-02-13 15:52:48 +0530

    add Ticket DTOs

---
+++ src/dtos/IssueTicketRequestDTO.java
@@ -0,0 +1,42 @@
+package dtos;
+
+import enums.VehicleType;
+
+public class IssueTicketRequestDTO {
+    private Long gateId;
+    private String vehicleNumber;
+    private VehicleType vehicleType;
+    private String ownerName;
+
+    public Long getGateId() {
+        return gateId;
+    }
+
+    public void setGateId(Long gateId) {
+        this.gateId = gateId;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+}
---
+++ src/dtos/IssueTicketResponseDTO.java
@@ -0,0 +1,34 @@
+package dtos;
+
+import enums.ResponseStatus;
+import models.Ticket;
+
+public class IssueTicketResponseDTO {
+    private Ticket ticket;
+    private ResponseStatus responseStatus;
+    private String failureMessage;
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public ResponseStatus getResponseStatus() {
+        return responseStatus;
+    }
+
+    public void setResponseStatus(ResponseStatus responseStatus) {
+        this.responseStatus = responseStatus;
+    }
+
+    public String getFailureMessage() {
+        return failureMessage;
+    }
+
+    public void setFailureMessage(String failureMessage) {
+        this.failureMessage = failureMessage;
+    }
+}

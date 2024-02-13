b7e0fcfd5af60cc1295f97641fee010f3a3d436a
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-02-13 22:08:16 +0530

    modified RandomSlotAssignmentStrategy

--- src/strategies/RandomSlotAssignmentStrategy.java
+++ src/strategies/RandomSlotAssignmentStrategy.java
@@ -2,31 +2,14 @@ package strategies;
 
 import enums.ParkingSlotStatus;
 import enums.VehicleType;
-import exceptions.ParkingLotNotFoundException;
 import models.ParkingFloor;
 import models.ParkingLot;
 import models.ParkingSlot;
-import repositories.ParkingLotRepository;
-
 import java.util.Optional;
 
 public class RandomSlotAssignmentStrategy implements SlotAssignmentStrategy {
-    private final ParkingLotRepository parkingLotRepository;
-
-    public RandomSlotAssignmentStrategy(ParkingLotRepository parkingLotRepository) {
-        this.parkingLotRepository = parkingLotRepository;
-    }
-
     @Override
-    public Optional<ParkingSlot> getSlot(VehicleType vehicleType, Long gateId) throws ParkingLotNotFoundException {
-        Optional<ParkingLot> parkingLotOptional = parkingLotRepository.getParkingLotByGateId(gateId);
-
-        if (parkingLotOptional.isEmpty()) {
-            throw new ParkingLotNotFoundException();
-        }
-
-        ParkingLot parkingLot = parkingLotOptional.get();
-
+    public Optional<ParkingSlot> getSlot(VehicleType vehicleType, ParkingLot parkingLot) {
         for (ParkingFloor parkingFloor : parkingLot.getParkingFloors()) {
             for (ParkingSlot parkingSlot : parkingFloor.getParkingSlots()) {
                 if (parkingSlot.getParkingSlotStatus().equals(ParkingSlotStatus.UNOCCUPIED)
